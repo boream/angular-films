2080da932f2327dc462811752f0e344c8c16a330
Alejandro Garrido <alejandro.garrido@boream.com>
2020-02-26 14:13:42 +0100

    Update film component styles

--- src/app/shared/components/film/film.component.ts
+++ src/app/shared/components/film/film.component.ts
@@ -5,15 +5,25 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './film.component.html',
   styleUrls: ['./film.component.scss']
 })
-export class FilmComponent {
+export class FilmComponent implements OnInit{
 
   @Input() public data: any;
 
+  @Input() isEven: boolean;
+
   @Output() remove = new EventEmitter();
 
-  public isDisabled = true;
+  public color = 'red';
 
   constructor() { }
+
+  ngOnInit(): void {
+    console.log(`La peli ${this.data.name} es ${this.isEven ? 'par' : 'impar'}`);
+    if (!this.isEven) {
+      this.color = '#fff';
+    }
+    // throw new Error('Method not implemented.');
+  }
 
   eliminarFilm() {
     this.remove.emit(this.data);
