10a0745ed48c9845a0bb0390574ca5e34602417f
Alejandro Garrido <alejandro.garrido@boream.com>
2020-03-04 12:49:11 +0100

    Finish reactive forms with cross validations

--- src/app/shared/components/film-form-reactive/film-form-reactive.component.ts
+++ src/app/shared/components/film-form-reactive/film-form-reactive.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+
+const URL_VALID =
+  /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/;
+
 
 @Component({
   selector: 'app-film-form-reactive',
@@ -20,13 +24,26 @@ export class FilmFormReactiveComponent implements OnInit {
     this.formGroup = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(4)]],
       gender: ['', Validators.required],
-      image: ['', Validators.required],
-      imdbUrl: ['', Validators.required],
-    });
+      image: ['', [Validators.required, this.validateUrl]],
+      imdbUrl: ['', [Validators.required, this.validateUrl]],
+    }/*, { validators: this.sameUrl }*/);
   }
 
   onSave(form) {
     this.save.emit(form.value);
     form.reset();
   }
+
+  private validateUrl(control: AbstractControl): { [key: string]: any } {
+    const url = control.value;
+    const correct = URL_VALID.test(url);
+    return !correct ? { url: true } : null;
+  }
+
+  /* Cross Field Validation: https://angular.io/guide/form-validation#cross-field-validation
+  private sameUrl(group: FormGroup): { [key: string]: any } {
+    const imdb = group.get('imdbUrl').value;
+    const image = group.get('image').value;
+    return imdb === image ? null : { sameUrl: true };
+  }*/
 }
