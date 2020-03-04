448ae652f0d5e7dd72c6a6feeffa5e3deefc34ed
Alejandro Garrido <alejandro.garrido@boream.com>
2020-03-04 11:19:53 +0100

    Finish reactive forms.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { FilmComponent } from './components/film/film.component';
 import { MyShadowDirective } from './directives/my-shadow.directive';
 import { CapitalizeFirstPipe } from './pipe/capitalize-first.pipe';
-import { FilmService } from '../services/film.service';
 import { FilmFormComponent } from './components/film-form/film-form.component';
 import { FilmFormReactiveComponent } from './components/film-form-reactive/film-form-reactive.component';
 
