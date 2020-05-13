befa73460633344ea7774224786c4588600dfe06
alexseik <alexseik@hotmail.com>
2020-05-13 10:33:50 +0200

    Login with koa server and jwt

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,11 @@ export class LoginComponent implements OnInit {
 
   formGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
+  constructor(
+    private formBuilder: FormBuilder, 
+    private http: HttpClient,
+    private router: Router, 
+    @Inject(LOCAL_STORAGE) private storage: StorageService) { }
 
   ngOnInit() {
     this.formGroup = this.formBuilder.group({
@@ -23,11 +29,13 @@ export class LoginComponent implements OnInit {
   onLogin(form) {
     // const update = Object.assign({}, this.film, form.value);
     console.log(form.value);
-    this.http.post('http://localhost:3000/auth/login', form.value).subscribe(response => {
+    this.http.post('http://localhost:3000/auth/login', form.value).subscribe((response: any) => {
       debugger
+      this.storage.set('token', response.token);
+      this.router.navigate(['/film']);
       console.log(response);
     })
-    
+
   }
 
 }
